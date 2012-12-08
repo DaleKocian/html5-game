cd8c9171399b58acb280ce5b952f85f62f964685
dalekocian <dale.kocian@gmail.com>
2012-12-07 20:21:42 -0600

    remove default.js - DK

--- src/js/default.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', init, false);
-var canvasBg = document.getElementById('canvasBg');
-var cntxtBg = canvasBg.getContext('2d');
-
-
-function init() {
-    cntxtBg.fillStyle = '#505050';
-    cntxtBg.fillRect(20, 100, 600, 600);
-}
