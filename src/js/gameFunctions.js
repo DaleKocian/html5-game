270ad921bd16440fcf48c4d76768b092334c1195
dalekocian <dale.kocian@gmail.com>
2012-12-08 21:31:30 -0600

    Make if else more efficient - DK

--- src/js/gameFunctions.js
+++ src/js/gameFunctions.js
@@ -166,11 +166,12 @@ function Enemy(enemy) {
 }
 
 function setWave() {
-    if ($('#wave').text() === WAVE + 1) {
-       enemyWaveCount = WAVE_1_COUNT;
-    } else if ($('#wave').text() === WAVE + 2) {
+    var waveText = $('#wave').text();
+    if (waveText === WAVE + 1) {
+        enemyWaveCount = WAVE_1_COUNT;
+    } else if (waveText === WAVE + 2) {
         enemyWaveCount = WAVE_2_COUNT;
-    } else if ($('#wave').text() === WAVE + 3) {
+    } else if (waveText === WAVE + 3) {
         enemyWaveCount = WAVE_3_COUNT;
     }
 }
