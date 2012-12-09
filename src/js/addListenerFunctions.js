e17c32a3620464784c6b34371bda82b12f6a536f
dalekocian <dale.kocian@gmail.com>
2012-12-09 04:54:28 -0600

    move functionality to method - DK

--- src/js/addListenerFunctions.js
+++ src/js/addListenerFunctions.js
@@ -27,7 +27,21 @@ function addCharacterSelectListener() {
         }
         $('#character-select-screen').hide('slow');
         $('#game').show('slow');
-        startGame()
+        startGame();
+    });
+}
+function addCharacterHoverListener() {
+    $('#charcterTable').find('img').on('hover', function () {
+        $('.arrow').removeClass('arrow');
+        $(this).parent().addClass('arrow');
+        var id = $(this).prop('id');
+        if(id === 'dale') {
+            $('#playerInfo').text(DALES_BIO);
+        } else if(id === 'triest') {
+            $('#playerInfo').text(TRIEST_BIO);
+        } else {
+            $('#playerInfo').text(LEONS_BIO);
+        }
     });
 }
 function addPauseResumeButtonListener() {
@@ -41,9 +55,9 @@ function addPauseResumeButtonListener() {
 }
 function addPauseResumeKeyListener() {
     $('body').keypress(function () {
-        if (keydown.p) {
+        if(keydown.p) {
             var $el = $('#resume-pause');
-            if ($el.text() === 'Resume') {
+            if($el.text() === 'Resume') {
                 resumeGame();
                 $el.text('Pause');
             } else {
@@ -54,10 +68,10 @@ function addPauseResumeKeyListener() {
     });
 }
 function addPlayAgainListener() {
-    $('#play-again').on('click', function() {
+    $('#play-again').on('click', function () {
         $(this).closest('#gameOver').hide('slow');
         $('#game').show('slow');
-//        score = INITIAL_SCORE;
+        //        score = INITIAL_SCORE;
         health = INITIAL_HEALTH;
         lives = INITIAL_LIVES;
         setZombiesToCreateAndKill();
--- src/js/default.js
+++ src/js/default.js
@@ -9,16 +9,5 @@ $(function () {
     addPauseResumeButtonListener();
     addPauseResumeKeyListener();
     addPlayAgainListener();
-    $('#charcterTable').find('img').on('hover', function () {
-        $('.arrow').removeClass('arrow');
-        $(this).parent().addClass('arrow');
-        var id = $(this).prop('id');
-        if (id === 'dale') {
-            $('#playerInfo').text(DALES_BIO);
-        } else if (id === 'triest') {
-            $('#playerInfo').text(TRIEST_BIO);
-        } else {
-            $('#playerInfo').text(LEONS_BIO);
-        }
-    });
+    addCharacterHoverListener();
 });
